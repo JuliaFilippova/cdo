65c6225431ed8addc4e67b86ff7af8434882b416
Julia Filippova <mega.yulia1996@mail.ru>
2020-02-28 22:00:55 +0300

    +

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
-// $(document).ready(function () {
-//     $('.slider-advantages').slick({
-//         dots:           true,
-//         infinite:       true,
-//         speed:          500,
-//         slidesToShow:   1,
-//         slidesToScroll: 1,
-//         arrows:         false,
-//         responsive:     [{
-//             breakpoint: 400,
-//             settings:   {
-//                 arrows:         false,
-//                 slidesToShow:   1,
-//                 slidesToScroll: 1
-//             }
-//         }]
-//     });
-// });
+// MODAL WINDOW
+//hamburger
+$('#menuToggle').on('click',function() {
+    $('.menu-modal').toggle();
+    $('#modal').toggle();
+});
+//close bg
+$('#modal').on('click',function() {
+    $('.menu-modal').hide();
+    $('#modal').hide();
+});
+
+
+// section menu hamburger dropdown
+$(document).ready(function () {
+    $('.hamburger-dropdown__drop').click(function () {
+        $('.hamburger-dropdown__drop').not(this).children('.menu-open').removeClass('active');
+        $(this).children('.menu-open').toggleClass("active");
+
+        if ($(this).parent().is('.hamburger-dropdown__open')) {
+            $(this).closest('.hamburger-dropdown').find('.hamburger-dropdown-content').slideUp();
+            $(this).closest('.hamburger-dropdown').removeClass('hamburger-dropdown__open');
+        } else {
+            $('.hamburger-dropdown-content').slideUp();
+            $('.hamburger-dropdown').removeClass('hamburger-dropdown__open');
+            $(this).closest('.hamburger-dropdown').find('.hamburger-dropdown-content').slideDown();
+            $(this).closest('.hamburger-dropdown').addClass('hamburger-dropdown__open');
+        }
+    });
+});
